4fa32eefa200fdb729c575de12df5bf689bb3a02
Green-Avocado <greenavocado@protonmail.com>
2021-10-11 06:08:07 -0700

    Task 4: added payload stubs

--- src/main/model/Payload.java
+++ src/main/model/Payload.java
@@ -1,4 +1,33 @@
 package model;
 
+import java.util.LinkedList;
+
 public class Payload {
+    private static boolean isAmd64 = true;
+
+    private final LinkedList<Gadget> gadgetChain;
+
+    public Payload() {
+        gadgetChain = new LinkedList<>();
+    }
+
+    public void addGadget(Gadget gadget) {
+        //TODO: STUB
+    }
+
+    public void removeGadget(int index) {
+        //TODO: STUB
+    }
+
+    public LinkedList<Gadget> viewPayload() {
+        return null; //TODO: STUB
+    }
+
+    public String outputScript() {
+        return null; //TODO: STUB
+    }
+
+    public static void setIsAmd64(boolean isAmd64) {
+        Payload.isAmd64 = isAmd64;
+    }
 }
