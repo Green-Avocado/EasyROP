9e8193533b49a860a46f5623623e8b7f34b5dac4
Green-Avocado <greenavocado@protonmail.com>
2021-10-12 02:22:11 -0700

    Task 4: implemented RopChain methods

--- src/main/model/RopChain.java
+++ src/main/model/RopChain.java
@@ -14,32 +14,65 @@ public class RopChain {
     // MODIFIES: this
     // EFFECTS: adds gadget to the gadget list at the specified index, returns true if successful
     public boolean addGadget(Gadget gadget, int index) {
-        return false; //TODO: STUB
+        if (index <= gadgetList.size()) {
+            gadgetList.add(index, gadget);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // REQUIRES index >= 0
     // MODIFIES: this
     // EFFECTS: replaces gadget at the specified index with the specified gadget, returns true if successful
     public boolean setGadget(Gadget gadget, int index) {
-        return false; //TODO: STUB
+        if (index < gadgetList.size()) {
+            gadgetList.set(index, gadget);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // REQUIRES: index >= 0
     // MODIFIES: this
     // EFFECTS: removes gadget from specified index, returns true if successful
     public boolean removeGadget(int index) {
-        return false; //TODO: STUB
+        if (index < gadgetList.size()) {
+            gadgetList.remove(index);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // EFFECTS: returns a string of Python commands to print the RopChain
     public String ropScript() {
-        return null; //TODO: STUB
+        String script = "";
+
+        for (Gadget gadget : gadgetList) {
+            String gadgetScript = gadget.gadgetScript();
+
+            if (!gadgetScript.isEmpty()) {
+                if (!script.isEmpty()) {
+                    script = script.concat("\n");
+                }
+
+                script = script.concat(gadget.gadgetScript());
+            }
+        }
+
+        return script;
     }
 
     // REQUIRES index >= 0
     // EFFECTS: returns the gadget at the specified index or null if no such Gadget exists
     public Gadget getGadget(int index) {
-        return null; //TODO: STUB
+        if (index < gadgetList.size()) {
+            return gadgetList.get(index);
+        } else {
+            return null;
+        }
     }
 
     // EFFECTS: returns the list of Gadgets
