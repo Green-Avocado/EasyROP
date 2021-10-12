9c7c6497bd3a7680815eb767fbdcd8caab304a16
Green-Avocado <greenavocado@protonmail.com>
2021-10-12 07:54:06 -0700

    Task 4: implemented InstructionsGadget test methods

--- src/test/model/InstructionsGadgetTest.java
+++ src/test/model/InstructionsGadgetTest.java
@@ -3,6 +3,9 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InstructionsGadgetTest {
@@ -15,25 +18,154 @@ class InstructionsGadgetTest {
 
     @Test
     void getScriptTest() {
+        gadget.setBase("exe");
+        gadget.add("ret", 0);
+
+        assertEquals("rop_exe.find_gadget(['ret',])[0]", gadget.getScript());
+
+        gadget.add("pop rdi", 0);
+
+        assertEquals("rop_exe.find_gadget(['pop rdi','ret',])[0]", gadget.getScript());
     }
 
     @Test
     void addInstructionTest() {
+        ArrayList<String> instructionList = new ArrayList<>();
+
+        for(int i = 0; i < 4; i++) {
+            String instruction = "ret";
+            instructionList.add(instruction);
+        }
+
+        // add to out of bounds index
+        assertFalse(gadget.add(instructionList.get(0), gadget.getLength() + 1));
+        assertEquals(0, gadget.getLength());
+
+        // add to empty
+        assertTrue(gadget.add(instructionList.get(2), 0));
+        assertEquals(1, gadget.getLength());
+        assertEquals(instructionList.subList(2,3), gadget.getInstructions());
+
+        // add to start of list
+        assertTrue(gadget.add(instructionList.get(0), 0));
+        assertEquals(2, gadget.getLength());
+        assertEquals(
+                Arrays.asList(instructionList.get(0), instructionList.get(2)),
+                gadget.getInstructions()
+        );
+
+        // add to middle of list
+        assertTrue(gadget.add(instructionList.get(1), 1));
+        assertEquals(3, gadget.getLength());
+        assertEquals(instructionList.subList(0, instructionList.size() - 1), gadget.getInstructions());
+
+        // add to end of list
+        assertTrue(gadget.add(instructionList.get(3), gadget.getLength()));
+        assertEquals(instructionList.size(), gadget.getLength());
+        assertEquals(instructionList, gadget.getInstructions());
     }
 
     @Test
     void setInstructionTest() {
+        ArrayList<String> instructionList = new ArrayList<>();
+        ArrayList<String> newInstructionList = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++) {
+            String instruction = "ret";
+            instructionList.add(instruction);
+            gadget.add(instruction, gadget.getLength());
+        }
+
+        for(int i = 0; i < 3; i++) {
+            String instruction = "pop rdi";
+            newInstructionList.add(instruction);
+        }
+
+        // replace out of bounds index
+        assertFalse(gadget.set(newInstructionList.get(0), gadget.getLength()));
+        assertEquals(instructionList, gadget.getInstructions());
+
+        // replace first
+        assertTrue(gadget.set(newInstructionList.get(0), 0));
+        assertEquals(
+                Arrays.asList(newInstructionList.get(0), instructionList.get(1), instructionList.get(2)),
+                gadget.getInstructions()
+        );
+
+        // replace middle
+        assertTrue(gadget.set(newInstructionList.get(1), 1));
+        assertEquals(
+                Arrays.asList(newInstructionList.get(0), newInstructionList.get(1), instructionList.get(2)),
+                gadget.getInstructions()
+        );
+
+        // replace last
+        assertTrue(gadget.set(
+                newInstructionList.get(gadget.getLength() - 1),
+                gadget.getLength() - 1
+        ));
+        assertEquals(
+                newInstructionList,
+                gadget.getInstructions()
+        );
     }
 
     @Test
     void removeInstructionTest() {
+        ArrayList<String> instructionList = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++) {
+            String instruction = "ret";
+            instructionList.add(instruction);
+            gadget.add(instruction, gadget.getLength());
+        }
+
+        // remove from out of bounds index
+        assertFalse(gadget.remove(gadget.getLength()));
+        assertEquals(3, gadget.getLength());
+        assertEquals(instructionList, gadget.getInstructions());
+
+        // remove from start of list
+        assertTrue(gadget.remove(0));
+        assertEquals(instructionList.size() - 1, gadget.getLength());
+        assertEquals(instructionList.subList(1, instructionList.size()), gadget.getInstructions());
+
+        gadget.add(instructionList.get(0), 0);
+
+        // remove from middle of list
+        assertTrue(gadget.remove(gadget.getLength() - 2));
+        assertEquals(instructionList.size() - 1, gadget.getLength());
+        assertEquals(Arrays.asList(instructionList.get(0), instructionList.get(instructionList.size() - 1)), gadget.getInstructions());
+
+        gadget.add(instructionList.get(1), 1);
+
+        // remove from end of list
+        assertTrue(gadget.remove(gadget.getLength() - 1));
+        assertEquals(instructionList.size() - 1, gadget.getLength());
+        assertEquals(instructionList.subList(0, instructionList.size() - 1), gadget.getInstructions());
     }
 
     @Test
     void getInstructionsTest() {
+        ArrayList<String> instructionList = new ArrayList<>();
+
+        assertEquals(instructionList, gadget.getInstructions());
+
+        for(int i = 0; i < 3; i++) {
+            String instruction = "ret";
+            instructionList.add(instruction);
+            gadget.add(instruction, gadget.getLength());
+
+            assertEquals(instructionList, gadget.getInstructions());
+        }
     }
 
     @Test
     void setBaseTest() {
+        gadget.setBase("exe");
+        assertEquals("exe", gadget.getBase());
+
+        gadget.setBase("libc");
+        assertEquals("libc", gadget.getBase());
     }
 }
