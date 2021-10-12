3e554843ea9320ff6742a958a5a0043003e8753d
Green-Avocado <greenavocado@protonmail.com>
2021-10-11 19:24:17 -0700

    Task 4: added method comments to RopChain

--- src/main/model/RopChain.java
+++ src/main/model/RopChain.java
@@ -6,34 +6,45 @@ public class RopChain {
     private int padding = 0;
     private final LinkedList<Gadget> gadgetList;
 
+    // EFFECTS: creates a new RopChain with an empty list of gadgets
     public RopChain() {
         gadgetList = new LinkedList<>();
     }
 
-    public void addGadget(Gadget gadget, int index) {
-        //TODO: STUB
+    // MODIFIES: this
+    // EFFECTS: adds gadget to the gadget list at the specified index, returns true if successful
+    public boolean addGadget(Gadget gadget, int index) {
+        return false; //TODO: STUB
     }
 
-    public void removeGadget(int index) {
-        //TODO: STUB
+    // MODIFIES: this
+    // EFFECTS: removes gadget from specified index, returns true if successful
+    public boolean removeGadget(int index) {
+        return false; //TODO: STUB
     }
 
-    public void replaceGadget(Gadget gadget, int index) {
-        //TODO: STUB
+    // MODIFIES: this
+    // EFFECTS: replaces gadget at the specified index with the specified gadget, returns true if successful
+    public boolean replaceGadget(Gadget gadget, int index) {
+        return false; //TODO: STUB
     }
 
+    // EFFECTS: returns the gadget at the specified index or null if no such Gadget exists
     public Gadget getGadget(int index) {
         return null; //TODO: STUB
     }
 
-    public LinkedList<Gadget> getGadgetList(int index) {
+    // EFFECTS: returns the list of Gadgets
+    public LinkedList<Gadget> getGadgetList() {
         return gadgetList;
     }
 
+    // EFFECTS: returns a string of Python commands to print the RopChain
     public String ropScript() {
         return null; //TODO: STUB
     }
 
+    // EFFECTS: returns the number of Gadgets in the RopChain
     public int getRopChainLength() {
         return gadgetList.size();
     }
