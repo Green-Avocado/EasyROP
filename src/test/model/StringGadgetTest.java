a3cfd0c4c7c3cc1dce48e57c22df9a5544837458
Green-Avocado <greenavocado@protonmail.com>
2021-10-12 06:26:33 -0700

    Task 4: implemented StringGadget test methods

--- src/test/model/StringGadgetTest.java
+++ src/test/model/StringGadgetTest.java
@@ -15,13 +15,30 @@ class StringGadgetTest {
 
     @Test
     void getScriptTest() {
+        gadget.setBase("exe");
+        gadget.setString("cat flag.txt");
+        assertEquals("next((exe.search(b\"cat flag.txt\"))", gadget.getScript());
+
+        gadget.setBase("libc");
+        gadget.setString("/bin/sh\\x00");
+        assertEquals("next((libc.search(b\"/bin/sh\"x00))", gadget.getScript());
     }
 
     @Test
     void setStringTest() {
+        gadget.setString("cat flag.txt");
+        assertEquals("cat flag.txt", gadget.getString());
+
+        gadget.setString("/bin/sh\"x00");
+        assertEquals("/bin/sh\"x00", gadget.getString());
     }
 
     @Test
     void setBaseTest() {
+        gadget.setBase("exe");
+        assertEquals("exe", gadget.getBase());
+
+        gadget.setBase("libc");
+        assertEquals("libc", gadget.getBase());
     }
 }
