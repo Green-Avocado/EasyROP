cc307c9c064d5d36caf7d25e4931a1b4937541fa
Green-Avocado <greenavocado@protonmail.com>
2021-10-12 06:41:10 -0700

    Task 4: implemented SymbolGadget test methods

--- src/test/model/SymbolGadgetTest.java
+++ src/test/model/SymbolGadgetTest.java
@@ -15,17 +15,43 @@ class SymbolGadgetTest {
 
     @Test
     void getScriptTest() {
+        gadget.setBase("exe");
+        gadget.setType("got");
+        gadget.setSymbol("puts");
+
+        assertEquals("pack(exe.got['puts'])", gadget.getScript());
+
+        gadget.setBase("libc");
+        gadget.setType("sym");
+        gadget.setSymbol("system");
+
+        assertEquals("pack(libc.sym['system'])", gadget.getScript());
     }
 
     @Test
     void setSymbolTest() {
+        gadget.setSymbol("puts");
+        assertEquals("puts", gadget.getSymbol());
+
+        gadget.setSymbol("system");
+        assertEquals("system", gadget.getSymbol());
     }
 
     @Test
     void setTypeTest() {
+        gadget.setType("got");
+        assertEquals("got", gadget.getType());
+
+        gadget.setType("sym");
+        assertEquals("sym", gadget.getType());
     }
 
     @Test
     void setBaseTest() {
+        gadget.setBase("exe");
+        assertEquals("exe", gadget.getBase());
+
+        gadget.setBase("libc");
+        assertEquals("libc", gadget.getBase());
     }
 }
