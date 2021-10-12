d8a6466deb7e2596624f710a603d1ddf5baa5f7f
Green-Avocado <greenavocado@protonmail.com>
2021-10-12 06:14:29 -0700

    Task 4: implemented AddressGadget test methods

--- src/test/model/AddressGadgetTest.java
+++ src/test/model/AddressGadgetTest.java
@@ -15,13 +15,25 @@ class AddressGadgetTest {
 
     @Test
     void getScriptTest() {
+        gadget.setBase("exe");
+        gadget.setAddress("0xdeadbeef");
+        assertEquals("pack(exe.address + 0xdeadbeef)", gadget.getScript());
     }
 
     @Test
     void setAddressTest() {
+        for(int i = 0; i <= 256; i += 16) {
+            gadget.setAddress(Integer.toHexString(i));
+            assertEquals(Integer.toHexString(i), gadget.getAddress());
+        }
     }
 
     @Test
     void setBaseTest() {
+        gadget.setBase("exe");
+        assertEquals("exe", gadget.getBase());
+
+        gadget.setBase("libc");
+        assertEquals("libc", gadget.getBase());
     }
 }
