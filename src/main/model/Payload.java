2c2409845b1dbc5760b446552d6fd02c798b85f0
Green-Avocado <greenavocado@protonmail.com>
2021-10-12 03:51:35 -0700

    Task 4: implemented Payload methods

--- src/main/model/Payload.java
+++ src/main/model/Payload.java
@@ -5,30 +5,54 @@ import java.util.LinkedList;
 public class Payload {
     private static boolean isAmd64 = true;
 
-    private final LinkedList<RopChain> ropList;
+    private final LinkedList<RopChain> ropChainList;
 
     // EFFECTS: creates a new Payload with an empty list of RopChains
     public Payload() {
-        ropList = new LinkedList<>();
+        ropChainList = new LinkedList<>();
     }
 
     // REQUIRES index >= 0
     // MODIFIES: this
     // EFFECTS: adds a RopChain to the Payload at the specified index, returns true if successful
     public boolean addRopChain(RopChain ropChain, int index) {
-        return false; //TODO: STUB
+        if (index <= ropChainList.size()) {
+            ropChainList.add(index, ropChain);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // REQUIRES index >= 0
     // MODIFIES: this
     // EFFECTS: removes a RopChain from the Payload at the specified index, returns true if successful
     public boolean removeRopChain(int index) {
-        return false; //TODO: STUB
+        if (index < ropChainList.size()) {
+            ropChainList.remove(index);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // EFFECTS: return
     public String payloadScript() {
-        return null; //TODO: STUB
+        String script = "";
+
+        for (RopChain ropChain : ropChainList) {
+            String ropScript = ropChain.ropScript();
+
+            if (!ropScript.isEmpty()) {
+                if (!script.isEmpty()) {
+                    script = script.concat("\n");
+                }
+
+                script = script.concat(ropScript);
+            }
+        }
+
+        return script;
     }
 
     // MODIFIES: this
@@ -45,16 +69,20 @@ public class Payload {
     // REQUIRES index >= 0
     // EFFECTS: returns the RopChain at the specified index
     public RopChain getRopChain(int index) {
-        return null; //TODO: STUB
+        if (index < ropChainList.size()) {
+            return ropChainList.get(index);
+        } else {
+            return null;
+        }
     }
 
     // EFFECTS: returns the list of RopChains
     public LinkedList<RopChain> getRopChainList() {
-        return ropList;
+        return ropChainList;
     }
 
     // EFFECTS: returns the number of RopChains in the Payload
-    public int getPayloadLength() {
-        return ropList.size(); //TODO: STUB
+    public int getLength() {
+        return ropChainList.size();
     }
 }
