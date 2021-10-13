5d7b3c2c97713b30e9dfcbea3d9ca5cf480ee8aa
Green-Avocado <greenavocado@protonmail.com>
2021-10-12 18:00:37 -0700

    Task 5: added empty test classes for editors

---
+++ src/test/ui/PayloadEditorTest.java
@@ -0,0 +1,20 @@
+package ui;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PayloadEditorTest {
+    private PayloadEditor payloadEditor;
+
+    @BeforeEach
+    void runBefore() {
+        payloadEditor = new PayloadEditor();
+    }
+
+    @Test
+    void getContextTest() {
+        // TODO
+    }
+}
---
+++ src/test/ui/RopChainEditorTest.java
@@ -0,0 +1,20 @@
+package ui;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RopChainEditorTest {
+    private RopChainEditor ropChainEditor;
+
+    @BeforeEach
+    void runBefore() {
+        ropChainEditor = new RopChainEditor();
+    }
+
+    @Test
+    void getContextTest() {
+        // TODO
+    }
+}
