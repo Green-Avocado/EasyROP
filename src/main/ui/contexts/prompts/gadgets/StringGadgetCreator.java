21a39ec5a5ba346ea8d1c6c1736f2d8f048bde01
Green-Avocado <greenavocado@protonmail.com>
2021-10-14 23:58:38 -0700

    Task 5: added some method comments

--- src/main/ui/contexts/prompts/gadgets/StringGadgetCreator.java
+++ src/main/ui/contexts/prompts/gadgets/StringGadgetCreator.java
@@ -9,16 +9,21 @@ import ui.contexts.prompts.util.AddExploitObjectToIndex;
 public class StringGadgetCreator extends PromptContext {
     private String base;
 
+    // EFFECTS: Creates a new StringGadgetCreator with no set base
     public StringGadgetCreator(ConsoleContext parentContext) {
         super(parentContext, "Base", "exe");
     }
 
+    // EFFECTS: Creates a new StringGadgetCreator with the given base
     public StringGadgetCreator(ConsoleContext parentContext, String base) {
         super(parentContext, "String", "/bin/sh\\x00");
 
         this.base = base;
     }
 
+    // EFFECTS: If base is null, set base as input.
+    //          Otherwise, create a new StringGadget with the existing base and input as the string,
+    //          passing this object to the new AddExploitObjectToIndex context.
     public ConsoleContext handleInputInternal(String input) {
         if (base == null) {
             return new StringGadgetCreator(getParentContext(), input);
--- src/main/ui/contexts/prompts/gadgets/SymbolGadgetCreator.java
+++ src/main/ui/contexts/prompts/gadgets/SymbolGadgetCreator.java
@@ -10,16 +10,19 @@ public class SymbolGadgetCreator extends PromptContext {
     private String base;
     private String type;
 
+    // EFFECTS: Creates a new SymbolGadgetCreator context with no base or type.
     public SymbolGadgetCreator(ConsoleContext parentContext) {
         super(parentContext, "Base", "exe");
     }
 
+    // EFFECTS: Creates a new SymbolGadgetCreator context with a set base and no type
     public SymbolGadgetCreator(ConsoleContext parentContext, String base) {
         super(parentContext, "Type", "sym");
 
         this.base = base;
     }
 
+    // EFFECTS: Creates a new SymbolGadgetCreator context with a set base and type
     public SymbolGadgetCreator(ConsoleContext parentContext, String base, String type) {
         super(parentContext, "Symbol", "system");
 
@@ -27,6 +30,9 @@ public class SymbolGadgetCreator extends PromptContext {
         this.type = type;
     }
 
+    // EFFECTS: If the base is null, set the base as input. Otherwise, if the type is null, set the type as input.
+    //          Otherwise, create a new SymbolGadget with the existing base and type, and with input as the symbol,
+    //          passing this object to the new AddExploitObjectToIndex context.
     public ConsoleContext handleInputInternal(String input) {
         if (base == null) {
             return new SymbolGadgetCreator(getParentContext(), input);
