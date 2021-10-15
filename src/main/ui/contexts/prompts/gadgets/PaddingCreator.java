c554f77b74161fee4e9874368b3e63d4801cb645
Green-Avocado <greenavocado@protonmail.com>
2021-10-15 00:46:04 -0700

    Task 5: added PaddingCreator method comments

--- src/main/ui/contexts/prompts/gadgets/PaddingCreator.java
+++ src/main/ui/contexts/prompts/gadgets/PaddingCreator.java
@@ -7,10 +7,13 @@ import ui.contexts.prompts.util.AddExploitObjectToIndex;
 
 // Represents a UI element that creates a new Padding object
 public class PaddingCreator extends PromptContext {
+
+    // EFFECTS: Creates a new PaddingCreator context with a given parentContext
     public PaddingCreator(ConsoleContext parentContext) {
         super(parentContext, "Padding length", "8");
     }
 
+    // EFFECTS: Creates a new Padding object with a given length
     public ConsoleContext handleInputInternal(String input) {
         return new AddExploitObjectToIndex(getParentContext(), new Padding(input));
     }
