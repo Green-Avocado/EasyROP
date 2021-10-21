6fee942e0bc3aad125e883b803b5ad9800755650
Green-Avocado <greenavocado@protonmail.com>
2021-10-21 14:22:46 -0700

    added PayloadCreatorTest methods

--- src/test/ui/contexts/prompts/PayloadCreatorTest.java
+++ src/test/ui/contexts/prompts/PayloadCreatorTest.java
@@ -1,4 +1,28 @@
 package ui.contexts.prompts;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ui.contexts.menus.PayloadEditor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class PayloadCreatorTest {
+    private PayloadCreator payloadCreator;
+
+    @BeforeEach
+    void runBefore() {
+        payloadCreator = new PayloadCreator();
+    }
+
+    @Test
+    void testConstructor() {
+        assertNull(payloadCreator.getParentContext());
+    }
+
+    @Test
+    void testHandleInput() {
+        assertEquals("payload", ((PayloadEditor) payloadCreator.handleInput("")).getCollection().getName());
+        assertEquals("testName", ((PayloadEditor) payloadCreator.handleInput("testName")).getCollection().getName());
+    }
 }
