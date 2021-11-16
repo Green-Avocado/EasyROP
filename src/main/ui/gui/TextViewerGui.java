1bba8a7bbb42950ce4679c6278a594e788831b00
Green-Avocado <greenavocado@protonmail.com>
2021-11-16 13:07:03 -0800

    add text viewer frame

---
+++ src/main/ui/gui/TextViewerGui.java
@@ -0,0 +1,20 @@
+package ui.gui;
+
+import javax.swing.*;
+
+public class TextViewerGui extends JFrame {
+    private String content;
+
+    public TextViewerGui(String title, String content) {
+        super(title);
+        this.content = content;
+
+        JLabel contentLabel = new JLabel(content);
+        add(contentLabel);
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
