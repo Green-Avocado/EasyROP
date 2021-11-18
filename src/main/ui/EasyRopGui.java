15fa260f15b0706757b1a8bfb70fc539763f1f8e
Green-Avocado <greenavocado@protonmail.com>
2021-11-18 00:39:23 -0800

    simplified entry point

--- src/main/ui/EasyRopGui.java
+++ src/main/ui/EasyRopGui.java
@@ -5,22 +5,8 @@ import ui.gui.SplashScreen;
 // EasyRop Application with graphical user interface
 public class EasyRopGui {
 
-    // EFFECTS: Creates a new EasyRop GUI application and launches the GUI
-    public EasyRopGui() {
-        run();
-    }
-
-    // MODIFIES: this.frame
-    // EFFECTS: Prompts the user to create the initial payload by entering a payload name.
-    //          If empty response, prompt again.
-    //          If cancelled, close the GUI.
-    //          If ok and non-empty response, set payload name and show GUI.
-    private void run() {
-        new SplashScreen();
-    }
-
     // EFFECTS: Starts the graphical user interface
     public static void main(String[] args) {
-        new EasyRopGui();
+        new SplashScreen();
     }
 }
