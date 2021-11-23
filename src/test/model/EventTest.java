c02f51855a3a16f345f3f14e5892393721e56040
Green-Avocado <greenavocado@protonmail.com>
2021-11-22 21:09:35 -0800

    Task 2: test Event

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -9,8 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for the Event class
@@ -19,6 +18,7 @@ public class EventTest {
     private Event event0;
     private Event event1;
     private Event event2;
+    private Event event3;
     private Date d;
 
     //NOTE: these tests might fail if time at which line (2) below is executed
@@ -26,11 +26,14 @@ public class EventTest {
     //run in same millisecond for this test to make sense and pass.
 
     @BeforeEach
-    public void runBefore() {
+    public void runBefore() throws InterruptedException {
         event0 = new Event("Sensor open at door");   // (1)
         event1 = new Event("Sensor open at door");
         event2 = new Event("Other event");
         d = Calendar.getInstance().getTime();   // (2)
+
+        Thread.sleep(1);
+        event3 = new Event("Sensor open at door");
     }
 
     @Test
@@ -45,12 +48,19 @@ public class EventTest {
     }
 
     @Test
-    public void testHashCode() throws InterruptedException {
+    public void testEquals() {
+        assertNotEquals(event0, null);
+        assertNotEquals(event0, EventLog.getInstance());
+
+        assertEquals(event1, event0);
+        assertNotEquals(event2, event0);
+        assertNotEquals(event3, event0);
+    }
+
+    @Test
+    public void testHashCode() {
         assertEquals(event1.hashCode(), event0.hashCode());
-
         assertNotEquals(event2.hashCode(), event0.hashCode());
-
-        Thread.sleep(1);
-        assertNotEquals(new Event("Sensor open at door").hashCode(), event0.hashCode());
+        assertNotEquals(event3.hashCode(), event0.hashCode());
     }
 }
